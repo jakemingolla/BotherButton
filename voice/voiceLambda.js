ce47a9cdec8b7f987568906f105ef32105e3e2d2
Jake mingolla <jakemingolla@gmail.com>
2015-11-18 12:30:24 -0500

    Modified call.xml

--- voice/voiceLambda.js
+++ voice/voiceLambda.js
@@ -3,13 +3,14 @@ var accountSid = 'AC848e9ac047dad5aef30d86cb580608c3';
 var authToken = '88e4f2d6ebade1e6b12579cbdca8b3c1'; 
 var fromNumber = '+17472325253';
 var toNumber = '+16173122509';
+var url = 'https://raw.githubusercontent.com/jakemingolla/bthr/master/voice/call.xml';
 
 var client = require('twilio')(accountSid, authToken)
 
 client.makeCall({
         from: fromNumber,
         to: toNumber,
-        url: 'http://ahoy.twilio.com/voice/api/demo'
+        url: url
 }, function(err, responseData) {
         console.log(responseData);
 });

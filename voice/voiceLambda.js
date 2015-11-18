9842473cbf103870d492209c2082900ab5b047d6
Jake mingolla <jakemingolla@gmail.com>
2015-11-18 12:54:27 -0500

    Fixed callback issues

--- voice/voiceLambda.js
+++ voice/voiceLambda.js
@@ -2,15 +2,37 @@
 var accountSid = 'AC848e9ac047dad5aef30d86cb580608c3'; 
 var authToken = '88e4f2d6ebade1e6b12579cbdca8b3c1'; 
 var fromNumber = '+17472325253';
-var toNumber = '+16173122509';
-var url = 'https://raw.githubusercontent.com/jakemingolla/bthr/master/voice/call.xml';
 
-var client = require('twilio')(accountSid, authToken)
 
-client.makeCall({
-        from: fromNumber,
-        to: toNumber,
-        url: url
-}, function(err, responseData) {
-        console.log(responseData);
-});
+exports.handler = function(event, context) {
+
+        console.log('Running event');
+
+        // Send a voice call to the number provided in the event data.
+        // End the lambda function when the send function completes.
+        sendCall(event.to, event.url,
+                 function(status) {
+                         context.done(null, status);
+                 }
+        );
+}
+
+// Sends a voie message using the Twilio API
+// PARAMETERS:
+//      to: Phone number to message.
+//      url: Url of the TwiML data for the call
+//      completedCallback(status): Callback for satus code once it has completed.
+function sendCall(to, url, completedCallback) {
+
+        var client = require('twilio')(accountSid, authToken);
+
+        client.makeCall({
+                from: fromNumber,
+                to: to,
+                url: url
+        }, function(err, responseData) {
+                console.log(responseData);
+                completedCallback('Voice call completed!');
+        });
+
+}
